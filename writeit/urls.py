89987c0f98cc82c56b0f7496f815b2cb63cffbc5
iyaky <roksolana.kordiuk.kn.2016@lpnu.ua>
2020-04-15 22:54:25 +0300

    Add blogs and edtailed blogs pages. Crosslinked and linked to home. Created register app

--- writeit/urls.py
+++ writeit/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
@@ -24,7 +24,7 @@ import home.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home.views.home, name='home'),
-    path('news/', news.views.news, name='home'),
+    path('news/', include('news.urls')),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
 urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
